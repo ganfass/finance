6b2b3a6ffb6216d6402777677c0c28cc3e28daae
ganaa <ganfass@gmail.com>
2021-05-02 17:26:02 +0800

    Event liseternuudiig uusgej ogloo

---
+++ app.js
@@ -0,0 +1,33 @@
+// Дэлгэцтэй ажиллах контроллер
+var uiController = (function(){
+    
+})();
+
+//Санхүүтэй ажиллах контроллер
+var financeController = (function(){})();
+
+
+//Програмыг холбогч контроллер
+var appController = (function(uiController,financeController){
+
+    var ctrlAddItem = function(){
+         //1. oruulah ugugdliig delgetsnees olj  avna.
+         //2. olj avsan ogogdluudee sanhuugiin controllert damjuulj tend hadgalna.
+         //3. olj avsan ogogdluudee web deeree tohiroh hesegt gargana
+         //4. Tusviig tootsoolno.
+         //5. etssiin uldegdel, tootsoog delgetsend gargana.
+
+    }
+    document.querySelector('.add__btn').addEventListener('click',function(){
+        console.log("clicked");
+        ctrlAddItem();
+    });
+
+    document.addEventListener('keypress',function(event){
+       
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
+ })(uiController, financeController);
